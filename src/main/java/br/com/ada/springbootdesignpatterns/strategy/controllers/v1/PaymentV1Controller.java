e635a442d2ec3fef3c1061a485a49027c067703e
Guilherme Lopes de Araujo <guilherme.xh@hotmail.com>
2022-11-30 01:12:04 -0300

    corrigindo o versionamento

--- src/main/java/br/com/ada/springbootdesignpatterns/strategy/controllers/v1/PaymentV1Controller.java
+++ src/main/java/br/com/ada/springbootdesignpatterns/strategy/controllers/v1/PaymentV1Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("v3/payments")
+@RequestMapping("v1/payments")
 public class PaymentV1Controller {
 
     @Autowired
--- src/main/java/br/com/ada/springbootdesignpatterns/strategy/controllers/v3/PaymentV3Controller.java
+++ src/main/java/br/com/ada/springbootdesignpatterns/strategy/controllers/v3/PaymentV3Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/payments")
+@RequestMapping("v3/payments")
 public class PaymentV3Controller {
     private final ApplicationContext context;
 
