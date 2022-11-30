f991374091c6b1d774af25eb43fb25b9f8c789fe
Guilherme Lopes de Araujo <guilherme.xh@hotmail.com>
2022-11-30 00:06:48 -0300

    initial commit

---
+++ src/test/java/br/com/ada/springbootdesignpatterns/SpringBootDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.springbootdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
