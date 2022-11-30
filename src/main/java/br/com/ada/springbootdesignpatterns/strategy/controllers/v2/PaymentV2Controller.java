104685d11f29f246762ae115d2bec1b5450107f1
Guilherme Lopes de Araujo <guilherme.xh@hotmail.com>
2022-11-30 01:00:21 -0300

    Strategy com Map

---
+++ src/main/java/br/com/ada/springbootdesignpatterns/strategy/controllers/v2/PaymentV2Controller.java
@@ -0,0 +1,27 @@
+package br.com.ada.springbootdesignpatterns.strategy.controllers.v2;
+
+import br.com.ada.springbootdesignpatterns.strategy.services.PaymentMethodStrategy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("v2/payments")
+public class PaymentV2Controller {
+
+    private final Map<String, PaymentMethodStrategy> paymentMethodStrategyMap;
+
+    public PaymentV2Controller(Map<String, PaymentMethodStrategy> paymentMethodStrategyMap) {
+        this.paymentMethodStrategyMap = paymentMethodStrategyMap;
+    }
+
+    @GetMapping("pay/{paymentMethod}")
+    public String pay(@PathVariable String paymentMethod) {
+        PaymentMethodStrategy paymentMethodStrategy = paymentMethodStrategyMap.get(paymentMethod);
+        return paymentMethodStrategy.pay();
+    }
+
+}
